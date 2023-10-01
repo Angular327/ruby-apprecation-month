aa225a80871c23501e4f6bc08c9e565fbb094728
milohk <30414356+Angular327@users.noreply.github.com>
2023-09-30 22:47:42 -0400

    Update to Day

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-  const countDownDate = new Date('October 1, 2023 00:00:00 GMT-05:00').getTime();
+  const countDownDate = new Date('October 29, 2023 00:00:00 GMT-05:00').getTime();
 
   const [countDown, setCountDown] = useState(
     countDownDate - new Date().getTime()
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <>
-      <h1>RUBY APPRECATION MONTH:</h1>
+      <h1>RUBY APPRECATION DAY:</h1>
       <br></br>
       <h2>Days: <div style={{color: 'pink'}}>{Math.floor(countDown / (1000 * 60 * 60 * 24))}</div>  Hours: <div style={{color: 'pink'}}>{Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))}</div> Minutes: <div style={{color: 'pink'}}>{Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60))}</div> Seconds: <div style={{color: 'pink'}}>{Math.floor((countDown % (1000 * 60)) / 1000)}</div></h2>
     </>
